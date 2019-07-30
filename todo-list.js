692bce40ceecbdb0162bf35fbe76b1fdbb00bd6e
Kayla Lawson <kaylarae7128@gmail.com>
2019-07-30 13:01:52 -0600

    first commit

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constructor (id, title, tasks, urgent) {
+    this.id = id;
+    this.title = title;
+    this.tasks = tasks || [];
+    this.urgent = urgent || false;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateToDo() {
+
+  }
+
+  updateTask() {
+
+  }
+}
